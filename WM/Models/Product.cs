f972f6d5f0dd141c803d4a94b502cc42314c460f
maksimovicn <nikolamaxa93@gmail.com>
2018-06-30 11:18:28 +0200

    Adding model for product

---
+++ WM/Models/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WM.Models
+{
+    public class Product
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Manufacterer { get; set; }
+        public decimal Price { get; set; }
+        public string Supplier { get; set; }
+        public string Category { get; set; }
+
+    }
+}
